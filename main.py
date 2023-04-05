cff4ef15e5fb449feed6723312ffb07e33af330b
MrPaivas <tiago.paiva@outlook.com.br>
2023-04-05 12:37:36 -0300

    implementado leitor de texto para acessibilidade

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import pyttsx3
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
+engine = pyttsx3.init()
 consulta_1 = []
 consulta_2 = []
+
+
+def texto_p_audio(texto):
+    mensagem = "Nova Consulta!"
+    lista = []
+    lista.append(mensagem)
+    lista.append(texto)
+    os.remove('./static/audio/audio.mp3')
+    engine.setProperty("voice", 0)
+    engine.setProperty("gender", "male")
+    engine.setProperty("rate", 200)
+    engine.save_to_file(lista, './static/audio/audio.mp3')
+    engine.runAndWait()
 
 
 @app.route('/')
@@ -26,6 +42,7 @@ def on_connect(data):
 @socketio.on('envia_form')
 def handle_form_submission(dados):
     consulta_1.append(dados)
+    texto_p_audio(dados)
     emit('atualizar_resultado', dados, broadcast=True)
 
 
